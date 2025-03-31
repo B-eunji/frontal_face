a323ea955912d7328868582f5ef4e7e28b8bad12
B-eunji <a369pej@gmail.com>
2025-03-31 22:40:42 +0900

    fix: index.html css 파일 변경

---
+++ src/scripts/camera/camera.js
@@ -0,0 +1,17 @@
+/* 모든 JS 파일 묶어주는 Vite 진입점 js */
+
+/* CSS */
+import './styles/style.css';
+
+/* JS */
+/* import './api/api.js'; 
+import './camera-capture.js';
+import './camera-stream.js';
+import './direction-arrow.js';
+import './direction-arrow';
+import './face-detection.js';
+import './face_landmark-draw.js';
+import './model-load.js';
+import './script.js'; */
+
+
