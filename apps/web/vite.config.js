7006f653c4709f8777b78d43592f0989cbf43581
B-eunji <a369pej@gmail.com>
2025-12-02 21:52:47 +0900

    feat: split project into web and api apps

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        camera: resolve(__dirname, '/src/camera/index.html')
+        camera: resolve(__dirname, 'camera.html')
       }
     }
   }
